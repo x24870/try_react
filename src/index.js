febb04fd5332c05beb2a7fc35c8bea9d4c4fcb71
pyk <x24870@gmail.com>
2020-11-08 15:36:46 +0800

    function component

--- src/index.js
+++ src/index.js
@@ -2,17 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => { this.props.onClick() }}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+//   render() {
+//     return (
+//       <button
+//         className="square"
+//         onClick={() => { this.props.onClick() }}
+//       >
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+// function component
+function Square(props){
+  return (
+    <button className="square" conClick={props.onClick}>
+      {props.value}
+    </button>
+  )
 }
 
 class Board extends React.Component {
