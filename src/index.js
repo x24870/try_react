7e38f1371964f363ad5d8afd2ba0eec73c0bf248
pyk <x24870@gmail.com>
2020-11-08 15:43:49 +0800

    taking turns

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ import './index.css';
 // function component
 function Square(props){
   return (
-    <button className="square" conClick={props.onClick}>
+    <button className="square" onClick={props.onClick}>
       {props.value}
     </button>
   )
@@ -29,6 +29,7 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
@@ -43,12 +44,15 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next Player is :' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
