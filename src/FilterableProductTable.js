5accebeb6cc9370c19d4b3e341c8803a0137977a
pyk <x24870@gmail.com>
2020-11-15 16:55:41 +0800

    build static UI and add style

--- src/FilterableProductTable.js
+++ src/FilterableProductTable.js
@@ -1,28 +1,57 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './FilterableProductTable.css';
 
-
-function SearchBar(props){
-    return (
-        <input type="text" placeholder="search for puducts"/>
-    )
+function ProductRow(props) {
+  return (
+    <div className="product">Product1</div>
+  );
 }
 
-export default class FilterableProductTalbe extends React.Component{
-    constructor(props){
-        super(props);
-    }
+function ProductCategoryRow(props) {
+  return (
+    <div className="product-category-row">
+      <span>Electronic</span>
+    </div>
+  );
+}
 
-    render(){
-        return(
-            <SearchBar/>
-        );
-    }
+function ProductTable(props) {
+  return (
+    <div className="product-table">
+      <div>
+        <span>Name</span>
+        <span>Price</span>
+      </div>
+      <ProductCategoryRow></ProductCategoryRow>
+      <ProductRow></ProductRow>
+    </div>
+  );
+}
+
+function SearchBar(props) {
+  return (
+      <input className="searchbar" type="text" placeholder="search for puducts" />
+  )
+}
+
+export default class FilterableProductTalbe extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="filterable-product-table-container">
+        <SearchBar />
+        <ProductTable />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-    <FilterableProductTalbe />,
-    document.getElementById('root')
+  <FilterableProductTalbe />,
+  document.getElementById('root')
 );
